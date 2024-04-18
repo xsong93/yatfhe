69109eabb4c24e93f19edf2fe8fc290f5c25e77b
Xintong Song <sweetice9cn@hotmail.com>
2024-04-18 20:02:48 +0800

    "trgswFunctionalBootstrapping" part57.22: todo: trlwe mult large const test review todos, TrlweMultConstant(todo: fix overflow issue), DecomposeOverBMultiStages(overflow support/wip), add second lvl decompose support for trgsw(wip), ntt32.cpp(wip), curr ntt/intt impl failed on small negative inputs, test decompose/ntt order (should have no diff), trgswFunctionalBootstrapping(need 2nd pass), accMulToBsk(debug), blindRotate(debug), controlMux(debug)

--- google_tests/trlwe_test.cpp
+++ google_tests/trlwe_test.cpp
@@ -128,79 +128,96 @@ TEST(TrlweAddSubMultiSampleTest, TrlweAddSubMultiSampleTest) {
 
 TEST(TrlweMultConstant, TrlweMultConstant) {
     YatfheParameters param {};
-    param.torusBase = 1 << 28;
+    param.torusBase = 1 << 3;
 
     TrlweKey trlweKey {param.k, param.N};
-    Trlwe trlwe1 {param.k, param.N};
-    TrlweDft trlweDft1 {param.k, param.N};
+    Trlwe trlwe {param.k, param.N};
+    TrlweDft trlweDft {param.k, param.N};
 
     trlweKeyGen(trlweKey);
 
-    std::vector<double> plain1(param.N);
+    // data gen
+    DoublePolynomial plain {param.N};
     std::vector<Torus> in1(param.N);
     for (auto i = 0; i < in1.size(); i++) {
-        plain1[i] = (double) genIntUniformDist(-param.torusBase / 4 + 1, param.torusBase / 4 - 1) / param.torusBase;
-        in1[i] = doubleToTorus32(plain1[i]);
+        plain.coeffs[i] = (double) genIntUniformDist(-param.torusBase / 4 + 1, param.torusBase / 4 - 1) / param.torusBase;
+        in1[i] = doubleToTorus32(plain.coeffs[i]);
     }
-    printArray(plain1, "plain1");
 
-    symEncTrlweMultiSample(trlwe1, trlweDft1, trlweKey, in1, param.lweStdDev);
-    printTrlweAB(trlwe1, "t1");
+    // enc
+    symEncTrlweMultiSample(trlwe, trlweDft, trlweKey, in1, param.lweStdDev);
+    printTrlweAB(trlwe, "trlwe");
 
-    DecomposedTrlwe d {param};
+    // decomp trlwe
+    DecomposedTrlwe lhs {param};
 //    for (auto l = 0; l < d.l; l++) {
 //        for (auto i = 0; i < in1.size(); i++) {
 //            auto decomposedMu = in1[i] << (param.torusBits - (i + 1) * param.radixBits);
 //
 //        }
 //    }
-    gadgetDecomposeTrlwe(d, trlwe1, param);
-    printDecomposedTrlweAB(d, "d");
+    gadgetDecomposeTrlwe(lhs, trlwe, param);
+    printDecomposedTrlweAB(lhs, "lhs");
 
-    // todo: fix overflow issue
-    Integer y = 5;
-    vector<Integer> yOr(d.l);
-    decomposeOverB(yOr, y, param);
-    printArray(yOr,"yOr");
-    vector<DecomposedData> dy2(d.l, DecomposedData(d.l));
-    for (auto i = 0; i < dy2.size(); i++) {
-        gadgetDecompose(dy2[i], yOr[i], param);
-        printArray(dy2[i].value, "dy2 level " + to_string(i));
-    }
+    // first decomp
+    // todo: decomposition overflow support
+    Integer y = 7987;
+//    vector<Integer> rhs(lhs.l);
+//    decomposeOverB(rhs, y, param);
+//    printArray(rhs,"rhs");
+//
+//    // second decomp
+//    vector<DecomposedData> mid(lhs.l, DecomposedData(lhs.l));
+//    for (auto i = 0; i < mid.size(); i++) {
+//        gadgetDecompose(mid[i], rhs[i], param);
+//        printArray(mid[i].value, "mid level " + to_string(i));
+//    }
 
-
-//    Trlwe out {param.k, param.N};
-    DecomposedTrlwe out {param};
+    // first recomp
+    DecomposedTrlwe recomp1 {param};
+//    for (auto j = 0; j < param.N; j++) {
+//        for (auto r = 0; r < param.k; r++) {
+//            for (auto l1 = 0; l1 < lhs.l; l1++) {
+//                for (auto l2 = 0; l2 < lhs.l; l2++) {
+//                    recomp1.rlwes[l1].a[r].coeffs[j] += lhs.rlwes[l1].a[r].coeffs[j] * mid[l1].value[l2];
+//                }
+//            }
+//        }
+//        for (auto l1 = 0; l1 < lhs.l; l1++) {
+//            for (auto l2 = 0; l2 < lhs.l; l2++) {
+//                recomp1.rlwes[l1].b.coeffs[j] += lhs.rlwes[l1].b.coeffs[j] * mid[l1].value[l2];
+//            }
+//        }
+//    }
     for (auto j = 0; j < param.N; j++) {
         for (auto r = 0; r < param.k; r++) {
-            for (auto l1 = 0; l1 < d.l; l1++) {
-                for (auto l2 = 0; l2 < d.l; l2++) {
-                    out.rlwes[l1].a[r].coeffs[j] += d.rlwes[l1].a[r].coeffs[j] * dy2[l1].value[l2];
-                }
+            for (auto l1 = 0; l1 < lhs.l; l1++) {
+                recomp1.rlwes[l1].a[r].coeffs[j] = lhs.rlwes[l1].a[r].coeffs[j] * y;
             }
         }
-        for (auto l1 = 0; l1 < d.l; l1++) {
-            for (auto l2 = 0; l2 < d.l; l2++) {
-                out.rlwes[l1].b.coeffs[j] += d.rlwes[l1].b.coeffs[j] * dy2[l1].value[l2];
-            }
+        for (auto l1 = 0; l1 < lhs.l; l1++) {
+            recomp1.rlwes[l1].b.coeffs[j] = lhs.rlwes[l1].b.coeffs[j] * y;
         }
     }
 
-    printDecomposedTrlweAB(out, "out");
-    Trlwe fi {param.k, param.N};
-    recomposeTrlwe(fi, out, param);
-    printTrlweAB(fi, "fi");
+    printDecomposedTrlweAB(recomp1, "recomp1");
 
+    // second recomp
+    Trlwe recomp2 {param.k, param.N};
+    recomposeTrlwe(recomp2, recomp1, param);
+    printTrlweAB(recomp2, "recomp2");
+
+    // dec
     DoublePolynomial dp {param.N};
-    TrlweDft trlweDft {param.k, param.N};
-    applyNttForAB(trlweDft, fi);
+    applyNttForAB(trlweDft, recomp2);
     symDecTrlwe(dp, trlweDft, trlweKey, param.torusBase);
+    printArray(plain.coeffs, "p0");
     printArray(dp.coeffs, "dp");
     DoublePolynomial tv {param.N};
-    for (auto i = 0; i < plain1.size(); i++) {
-        tv.coeffs[i] = plain1[i] * y;
+    for (auto i = 0; i < plain.coeffs.size(); i++) {
+        tv.coeffs[i] = torus32ToDouble(doubleToTorus32(plain.coeffs[i] * y));
     }
-    printArray(tv.coeffs, "tv");
+    printArray(tv.coeffs, "p1");
 
     printBanner("TrlweMultConstant");
 }
