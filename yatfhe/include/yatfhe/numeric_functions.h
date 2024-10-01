846e1f47f39fdc41063418596e13cf7c80e3c08a
Xintong Song <sweetice9cn@hotmail.com>
2024-10-01 22:30:42 +0800

    [DB] correct all mod arith in *.h

--- yatfhe/include/yatfhe/numeric_functions.h
+++ yatfhe/include/yatfhe/numeric_functions.h
@@ -15,7 +15,6 @@ extern random_device rd;
 extern mt19937 rng;
 extern uniform_int_distribution<Binary> binaryDistrib;
 extern uniform_int_distribution<Torus> uniformTorusDistrib;
-static const int64_t twoP32 = INT64_C(1) << 32; // 2^32
 
 int calLogBase2(int N);
 
