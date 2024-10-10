9935f95d5aa67068443d518e69116f923f45de5a
xsong93 <xs708@nyu.edu>
2024-10-10 17:49:45 +0800

    [TP] BLIND_ROT_APPROX_CRT, BLIND_ROT_APPROX_CRT_NTT

--- google_tests/blindrot_test.cpp
+++ google_tests/blindrot_test.cpp
@@ -76,7 +76,7 @@ TEST(BlindRot, BlindRot) {
     printBanner("BlindRot");
 }
 
-TEST(BlindRot, BlindRotMCRT) {
+TEST(BlindRot, BLIND_ROT_APPROX_CRT) {
     YatfheParameters param {};
     param.n = 64;
     param.N = 32;
@@ -88,10 +88,8 @@ TEST(BlindRot, BlindRotMCRT) {
     TrgswKey trgswKey {param};
     TrlweKey& trlweKey = trgswKey.trlweKey;
     trlweKeyGen(trlweKey);
-    BootstrappingKey bsk {param};
     BootstrappingKeyCRT bsKeyCRT{param};
-    bootstrappingKeyGen(bsk, param, trgswKey, tlweKey);
-    bootstrappingKeyMCRTDecomp(bsKeyCRT, bsk, param);
+    bootstrappingKeyGenApproxCRT(bsKeyCRT, param, trgswKey, tlweKey);
 
     // data gen
     Trlwe in2 {param.k, param.N};
@@ -132,24 +130,92 @@ TEST(BlindRot, BlindRotMCRT) {
     symDecTrlweToInt(rotInP, rotIn, trlweKey, param.torusBase);
     printArray(rotInP.coeffs, "expect");
 
-    blindRotate(in2, bsk, sTlwe, param);
     trlweMCRTDecomp(rotCRT, in2, param);
-    blindRotateMCRT(rotCRT, bsKeyCRT, sTlwe, param);
+    blindRotateApproxCRT(rotCRT, bsKeyCRT, sTlwe, param);
+    trlweMCRTToCRT(rotCRT, param);
     trlweCRTRecomp(resMCRT, rotCRT, param);
 
     // dec
-    IntPolynomial decP {param.N};
     IntPolynomial decMP {param.N};
-    symDecTrlweToInt(decP, in2, trlweKey, param.torusBase);
     symDecTrlweToInt(decMP, resMCRT, trlweKey, param.torusBase);
-    printArray(decP.coeffs, "real");
     printArray(decMP.coeffs, "realMCRT");
 
     //verify
-    for (auto i = 0; i < decP.N; i++) {
-        ASSERT_EQ(rotInP.coeffs[i], decP.coeffs[i]);
+    for (auto i = 0; i < decMP.N; i++) {
+        ASSERT_EQ(rotInP.coeffs[i], decMP.coeffs[i]);
     }
-    printBanner("BlindRotMCRT");
+    printBanner("BLIND_ROT_APPROX_CRT");
+}
+
+TEST(BlindRot, BLIND_ROT_APPROX_CRT_NTT) {
+    YatfheParameters param {};
+    param.n = 64;
+//    param.N = 32;
+    yatfheInit(param);
+
+    // key gen
+    TlweKey tlweKey {param.n, param.lweStdDev};
+    lweKeyGen(tlweKey);
+    TrgswKey trgswKey {param};
+    TrlweKey& trlweKey = trgswKey.trlweKey;
+    trlweKeyGen(trlweKey);
+    BootstrappingKeyCRT bsKeyCRT{param};
+    bootstrappingKeyGenApproxCRT(bsKeyCRT, param, trgswKey, tlweKey);
+
+    // data gen
+    Trlwe in2 {param.k, param.N};
+    Trlwe resMCRT {param.k, param.N};
+    std::vector<Trlwe8> rotCRT(param.d, Trlwe8{param.k, param.N});
+    IntPolynomial plain {param.N}; // Z/pZ
+    TorusPolynomial plainT {param.N};
+    for (auto i = 0; i < plain.N; i++) {
+        plain.coeffs[i] = genIntUniformDist(-param.torusBase / 2, param.torusBase / 2 - 1);
+        plainT.coeffs[i] = modSwitchToTorus32(plain.coeffs[i], param.torusBase);
+    }
+    symEncTrlweMultiSample(in2, trlweKey, plainT.coeffs);
+    printTrlweAB(in2, "input");
+
+    // pre dec
+    IntPolynomial decIn {param.N};
+    symDecTrlweToInt(decIn, in2, trlweKey, param.torusBase);
+    printArray(plain.coeffs, "plain");
+    printArray(decIn.coeffs, "decIn");
+
+    // rots gen
+    ScaledTlwe sTlwe {param.N * 2, param.n};
+    for (auto i = 0 ; i < sTlwe.n; i++) {
+        sTlwe.a[i] = genIntUniformDist(TORUS_MIN, TORUS_MAX);
+    }
+
+    // test data gen
+    int rot = 0;
+    IntPolynomial rotInP {param.N};
+    Trlwe rotIn {param.k, param.N};
+    for (auto i = 0 ; i < param.n; i++) {
+        if (tlweKey.s[i] == 1) {
+            rot += sTlwe.a[i];
+        }
+    }
+    cout << "rot:" << rot << endl;
+    trlweRotate(rotIn, in2, rot);
+    symDecTrlweToInt(rotInP, rotIn, trlweKey, param.torusBase);
+    printArray(rotInP.coeffs, "expect");
+
+    trlweMCRTDecomp(rotCRT, in2, param);
+    blindRotateApproxCRTNtt(rotCRT, bsKeyCRT, sTlwe, param);
+    trlweMCRTToCRT(rotCRT, param);
+    trlweCRTRecomp(resMCRT, rotCRT, param);
+
+    // dec
+    IntPolynomial decMP {param.N};
+    symDecTrlweToInt(decMP, resMCRT, trlweKey, param.torusBase);
+    printArray(decMP.coeffs, "realMCRT");
+
+    //verify
+    for (auto i = 0; i < decMP.N; i++) {
+        ASSERT_EQ(rotInP.coeffs[i], decMP.coeffs[i]);
+    }
+    printBanner("BLIND_ROT_APPROX_CRT_NTT");
 }
 
 TEST(BlindRot, BlindRotLut) {
