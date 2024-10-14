1c6bd9652672d883a1119c2ab938875a3affc3a1
xsong93 <xs708@nyu.edu>
2024-10-14 17:18:54 +0800

    [CI] remove lock for time_counter.h

--- yatfhe/include/yautil/time_counter.h
+++ yatfhe/include/yautil/time_counter.h
@@ -7,24 +7,17 @@
 #include <ctime>
 #include <chrono>
 #include <iostream>
-#include <mutex>
 
 using namespace std::chrono;
-//extern time_point<high_resolution_clock> timeGlobal;
 
 class TimeCounter {
 private:
     time_point<high_resolution_clock> time{};
-    std::mutex mux;
 
 public:
     static time_point<high_resolution_clock> timeGlobal;
 
     TimeCounter();
-
-    void lock();
-
-    void unlock();
 
     void resetTime();
 
@@ -44,11 +37,5 @@ public:
     CODE;                                                                 \
     TimeCounter::printTime(MSG, TimeCounter::timeGlobal);
 
-#define COUNT_TIME_LOCK(MSG, T, CODE) \
-    T.lock();                    \
-    T.resetTime();               \
-    CODE;                        \
-    T.printTime(MSG);            \
-    T.unlock();
 
 #endif //HLS_YATFHE_TIME_COUNTER_H
