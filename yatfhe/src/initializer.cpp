4d45514f91b6f05e11094d44c96cbccf6c87ef1b
xsong93 <xs708@nyu.edu>
2025-03-03 15:29:52 +0800

    [DB] fix ntt_test data init prob

--- yatfhe/src/initializer.cpp
+++ yatfhe/src/initializer.cpp
@@ -2,6 +2,7 @@
 // Created by Xintong Song on 2024/5/23.
 //
 #include "yautil/initializer.h"
+#include "yatfhe/ntt14.h"
 #include "yatfhe/ntt24.h"
 #include "yatfhe/ntt64.h"
 #include "yautil/time_counter.h"
@@ -40,6 +41,7 @@ void yatfheInit(YatfheParameters& param) {
     TORUS_MIN = INT_MIN_VALUE;
     initGlobalParamsNtt64(param.N);
     initGlobalParamsNtt24(param.N);
+    initGlobalParamsNtt14(param.N);
     for (size_t d = 0; d < param.dl; d++) {
         auto& dh = param.dh;
         param.taoU[dh + d] = static_cast<int>((modInverse(param.qLow / param.ql[d], param.ql[d])));
