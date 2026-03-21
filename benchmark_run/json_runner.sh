ffb2eb8dcf8859241fcdd71f829bd7a1272ca40e
xsong93 <xs708@nyu.edu>
2026-03-21 12:20:43 +0800

    edit ben

--- benchmark_run/json_runner.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-BENCHMARK_EXEC="bench_readkey"
-OUTPUT_PREFIX="result_9950x3d"
-
-# Run the benchmark
-"./$BENCHMARK_EXEC" \
-    --benchmark_out="${OUTPUT_PREFIX}_${BENCHMARK_EXEC}.json" \
-    --benchmark_out_format=json
